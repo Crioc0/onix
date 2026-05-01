08e6b000546f40d643e25ae4b7ce9798c9d1b758
Денис <pirrekd@mail.ru>
2026-05-01 15:41:17 +0300

    Верстка с блоком advantages включительно

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import headerHtml from './components/header.html?raw';
-document.getElementById('header').innerHTML = headerHtml;
+import headerHtml from "./components/header.html?raw";
+import heroHtml from "./components/hero.html?raw";
+import 'flowbite';
+// document.getElementById('header').innerHTML = headerHtml;
+// document.getElementById('hero').innerHTML = heroHtml;
