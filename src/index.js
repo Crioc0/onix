f8a81d668fda7c8b9b9b48978e86cd8f110665b5
Денис <pirrekd@mail.ru>
2026-05-03 15:54:03 +0300

    Убрал неиспользуемое

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import headerHtml from "./components/header.html?raw";
-import heroHtml from "./components/hero.html?raw";
 import 'flowbite';
-// document.getElementById('header').innerHTML = headerHtml;
-// document.getElementById('hero').innerHTML = heroHtml;
